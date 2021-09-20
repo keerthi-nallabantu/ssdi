8553396eb81ca058a13ec2f7968a96d32a79e1a4
keerthi-nallabantu <91036134+keerthi-nallabantu@users.noreply.github.com>
2021-09-19 22:35:43 -0400

    Add files via upload

---
+++ Spike2.js
@@ -0,0 +1,22 @@
+var http = require('http');
+http.createServer(function (req, res) {
+
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.end('Hello World I am Keerthi');
+ }).listen(4200);
+
+
+const {MongoClient} = require("mongodb");
+const url = 'mongodb://localhost:27017/';
+const databasename = "DemoDB";
+MongoClient.connect(url , {useNewUrlParser: true})
+.then(client => {
+    const connect = client.db(databasename);
+    const collection = connect.collection("DemoCollection");
+    
+    collection.find({}).toArray().then((ans) => {
+        console.table(ans);
+    })
+    }).catch((err) => {
+        console.log(err.message);
+    })
